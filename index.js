1078ffa36cca6b55089e373e24fcbdb96a83f815
vanyaVityunin <101450230+vanyaVityunin@users.noreply.github.com>
2022-03-12 13:18:45 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+class Blockchain(object):	
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+ 	
+    def new_block(self):
+        # Создает новый Блок и добавляет его к цепочке
+        pass
+
+ 	
+    def new_transaction(self):
+        # Добавляет новую транзакцию к списку транзакций
+        pass
